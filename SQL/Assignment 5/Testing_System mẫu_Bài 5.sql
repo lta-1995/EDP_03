bcbdd79833b7581d25695e5ee8be581f61e30c96
lta-1995 <tuananh9895@gmail.com>
2021-08-09 00:44:46 +0700

    Assignment 5 submit

---
+++ SQL/Assignment 5/Testing_System mẫu_Bài 5.sql
@@ -0,0 +1,376 @@
+/* Lenh xoa Database */
+DROP DATABASE IF EXISTS `Testing_System_1`;
+/* Lenh tao Database */
+CREATE DATABASE IF NOT EXISTS `Testing_System_1`;
+/* Lenh su dung Database */
+USE `Testing_System_1`;
+
+
+/*------------------- Create table ----------------------- */
+
+/* Lenh xoa bang */
+DROP TABLE IF EXISTS `Department`;
+/* Len tao bang va cac cot du lieu cua bang--*/
+CREATE TABLE `Department`
+(
+    `DepartmentID`   TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `DepartmentName` VARCHAR(50) NOT NULL UNIQUE KEY
+);
+Select * From `Department`;
+
+DROP TABLE IF EXISTS `Position`;
+CREATE TABLE IF NOT EXISTS `Position`
+(
+    `PositionID`   TINYINT UNSIGNED AUTO_INCREMENT,
+    `PositionName` ENUM ('Dev1', 'Dev2', 'Test', 'Scrum Master', 'PM'),
+    CONSTRAINT pk_1 PRIMARY KEY (PositionID)
+);
+
+DROP TABLE IF EXISTS `Account`;
+CREATE TABLE IF NOT EXISTS `Account`
+(
+    `AccountID`    MEDIUMINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `Email`        VARCHAR(100) UNIQUE,
+    `Username`     VARCHAR(50) NOT NULL,
+    `Fullname`     VARCHAR(50),
+    `DepartmentID` TINYINT UNSIGNED,
+    `PossitionID`  TINYINT UNSIGNED,
+    `CreateDate`   DATETIME ,
+    CONSTRAINT fk_dp_id FOREIGN KEY (`DepartmentID`) REFERENCES `Department` (`DepartmentID`) ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT fk_ps_id FOREIGN KEY (`PossitionID`) REFERENCES `Position` (`PositionID`) ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+DROP TABLE IF EXISTS `Group`;
+CREATE TABLE IF NOT EXISTS `Group`
+(
+    `GroupID`    MEDIUMINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `GroupName`  VARCHAR(50) NOT NULL,
+    `CreatorID`  MEDIUMINT UNSIGNED,
+    `CreateDate` DATETIME
+);
+
+DROP TABLE IF EXISTS `GroupAccount`;
+CREATE TABLE IF NOT EXISTS `GroupAccount`
+(
+    `GroupID`   MEDIUMINT,
+    `AccountID` MEDIUMINT,
+    `JoinDate`  DATETIME DEFAULT NOW()
+);
+
+DROP TABLE IF EXISTS `TypeQuestion`;
+CREATE TABLE IF NOT EXISTS `TypeQuestion`
+(
+    `TypeID`   TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `TypeName` VARCHAR(50)
+);
+
+DROP TABLE IF EXISTS `CategoryQuestion`;
+CREATE TABLE IF NOT EXISTS `CategoryQuestion`
+(
+    `CategoryID`   TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `CategoryName` ENUM ('Java', 'SQL', '.NET', 'Ruby', 'Python', 'NodeJS' , 'HTML', 'CSS', 'JavaScript')
+);
+
+DROP TABLE IF EXISTS `Question`;
+CREATE TABLE IF NOT EXISTS `Question`
+(
+    `QuestionID` TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `Content`    VARCHAR(50),
+    `CategoryID` TINYINT UNSIGNED,
+    `TypeID`     TINYINT UNSIGNED,
+    `CreatorID`  MEDIUMINT UNSIGNED,
+    `CreateDate` DATETIME ,
+    CONSTRAINT fk_q1 FOREIGN KEY (`CreatorID`) REFERENCES `Account` (`AccountID`) ON UPDATE CASCADE ON DELETE SET NULL,
+    CONSTRAINT fk_q2 FOREIGN KEY (`CategoryID`) REFERENCES `CategoryQuestion` (`CategoryID`) ON UPDATE CASCADE ON DELETE SET NULL,
+    CONSTRAINT fk_q3 FOREIGN KEY (`TypeID`) REFERENCES `TypeQuestion` (`TypeID`) ON UPDATE CASCADE ON DELETE SET NULL
+);
+
+DROP TABLE IF EXISTS `Answer`;
+CREATE TABLE IF NOT EXISTS `Answer`
+(
+    `AnswerID`   TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `Content`    VARCHAR(50),
+    `QuestionID` TINYINT UNSIGNED,
+    `isCorrect`  BIT, 
+    CONSTRAINT fk_qid FOREIGN KEY (`QuestionID`) REFERENCES `Question` (`QuestionID`) ON UPDATE CASCADE ON DELETE SET NULL
+);
+
+DROP TABLE IF EXISTS `Exam`;
+CREATE TABLE IF NOT EXISTS `Exam`
+(
+    `ExamID`     TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `Code`       VARCHAR(20) NOT NULL,
+    `Title`      VARCHAR(50) NOT NULL,
+    `CategoryID` TINYINT UNSIGNED,
+    `Duration`   TINYINT,
+    `CreatorID`  MEDIUMINT UNSIGNED,
+    `CreateDate` DATETIME ,
+    CONSTRAINT fk_ex_1 FOREIGN KEY (CreatorID) REFERENCES Account (AccountID) ON UPDATE CASCADE ON DELETE NO ACTION,
+    CONSTRAINT fk_ex_2 FOREIGN KEY (CategoryID) REFERENCES CategoryQuestion (CategoryID) ON UPDATE CASCADE ON DELETE SET NULL
+);
+
+
+DROP TABLE IF EXISTS `ExamQuestion`;
+CREATE TABLE IF NOT EXISTS `ExamQuestion`
+(
+    `ExamID`     TINYINT UNSIGNED,
+    `QuestionID` TINYINT UNSIGNED,
+    CONSTRAINT fk_eq1 FOREIGN KEY (ExamID) REFERENCES Exam (ExamID),
+    CONSTRAINT fk_eq2 FOREIGN KEY (QuestionID) REFERENCES Question (QuestionID)
+);
+
+
+/* ---------------------- INSERT DATA ---------------------------- */
+/* INSERT DATA bang Department */
+INSERT INTO `Department`(`DepartmentName`)
+VALUES ('Phong Ky Thuat 1'),
+       ('Phong Ky Thuat 2'),
+       ('Phong Dev 1'),
+       ('Phong Dev 2'),
+       ('Phong Sale'),
+       ('Phong Marketing'),
+       ('Phong Giam Doc'),
+       ('Phong Tai Chinh');
+
+/* Lenh sua bang `Position` sua kieu du lieu cot `PositionName` */
+ALTER TABLE `Position`
+    MODIFY COLUMN `PositionName` ENUM ('Dev1', 'Dev2', 'PM', 'Leader', 'Scrum Master' , 'Test');
+/* INSERT DATA bang Position */
+INSERT INTO `Position`(`PositionName`)
+VALUES ('Dev1'),
+       ('Dev2'),
+       ('Test'),
+       ('PM'),
+       ('Leader'),
+       ('Scrum Master');
+
+/* lenh sua bang xoa khoa ngoai foreign key*/
+ALTER TABLE `Account`
+    DROP CONSTRAINT `fk_dp_id`,
+    DROP CONSTRAINT `fk_ps_id`;
+/* INSERT DATA bang Account */
+INSERT INTO `Account`(`Email`, `Username`, `Fullname`, `DepartmentID`, `PossitionID`, `CreateDate`)
+VALUES ('vti_account1@vtiacademy.com', 'vti1', 'Nguyen Van Tinh', 1, 1, '2019-12-01'),
+       ('vti_account2@vtiacademy.com', 'vti2', 'Trinh Hoai Linh', 1, 2, '2020-12-01'),
+       ('vti_account3@vtiacademy.com', 'vti3', 'Nguyen Van Test', 1, 1, '2020-07-01'),
+       ('vti_account4@vtiacademy.com', 'vti4', 'Tran Van Tinh', 1, 2, '2019-09-01'),
+       ('vti_account5@vtiacademy.com', 'account5', 'Ho Van Tinh', 3, 2, '2021-07-01'),
+       ('vti_account6@vtiacademy.com', 'account_vti6', 'Cao Thai Son', 3, 1, NOW()),
+       ('vti_7@vtiacademy.com', 'account_vti7', 'Tam Thất Tùng', 3, 3, '2020-10-01'),
+       ('vti_8@vtiacademy.com', 'account_vti8', 'Nguyen Quynh Thu', 3, 4, '2019-04-01'),
+       ('vti_9@vtiacademy.com', 'account_vti9', 'Tran Kim Tuyen', 2, 1, NOW()),
+       ('vti_account10@vtiacademy.com', 'account_vti10', 'Nguyen Ba Dao', 1, 5, '2019-10-01'),
+       ('vti_account11@vtiacademy.com', 'account_vti11', 'Nguyen Van Binh', 1, 3, '2020-12-01');
+
+/* INSERT DATA bang Group */
+INSERT INTO `Group`(`GroupName`, `CreatorID`, CreateDate)
+VALUES ('Nhom 1', '3', '2021-04-03'),
+       ('Nhom 2', '3', '2019-01-03'),
+       ('Nhom 3', '2', '2020-04-03'),
+       ('Nhom 4', '1', NOW()),
+       ('Nhom 5', '3', '2021-06-03'),
+       ('Nhom 6', '1', '2020-04-03'),
+       ('Nhom 7', '5', '2021-04-03'),
+       ('Nhom 8', '5', '2019-05-03'),
+       ('Nhom 9', '3', '2019-01-03'),
+       ('Nhom 10', '1', NOW());
+
+/* INSERT DATA bang GroupAccount */
+INSERT INTO `GroupAccount`(`GroupID`, `AccountID`, `JoinDate`)
+VALUES ('1', '1', '2021-06-01'),
+       ('1', '3', '2020-01-01'),
+       ('1', '2', NOW()),
+       ('1', '4', '2021-06-01'),
+       ('2', '1', '2021-06-01'),
+       ('2', '10', '2019-05-01'),
+       ('5', '1', '2021-06-01'),
+       ('5', '3', '2020-01-01'),
+       ('5', '4', '2021-07-01'),
+       ('3', '1', '2021-06-01'),
+       ('9', '2', '2021-06-01'),
+       ('10', '1', NOW());
+
+/* INSERT DATA bang TypeQuestion */
+INSERT INTO `TypeQuestion`(`TypeName`)
+VALUES ('Trac nghiem'),
+       ('Tu Luan');
+
+/* INSERT DATA bang CategoryQuestion */
+/*`CategoryName` ENUM ('Java', 'SQL', '.NET', 'Ruby', 'Python', 'NodeJS' , 'HTML', 'CSS', 'JavaScript')*/
+INSERT INTO `CategoryQuestion` (`CategoryName`)
+VALUES ('Java'),
+       ('SQL'),
+       ('HTML'),
+       ('CSS '),
+       ('.NET'),
+       ('Python'),
+       ('Ruby'),
+       ('JavaScript');
+
+/* Lenh sua bang `Question` Xoa rang buoc khoa ngoai Foreign Key */
+ALTER TABLE `Question`
+    DROP CONSTRAINT `fk_q1`,
+    DROP CONSTRAINT `fk_q2`,
+    DROP CONSTRAINT `fk_q3`;
+/* INSERT DATA bang Question */
+INSERT INTO `Question`(`Content`, CategoryID, TypeID, CreatorID, CreateDate)
+VALUES ('Câu hỏi SQL 1', 2, 2, 1, '2021-04-01'),
+       ('Câu hỏi SQL 2', 2, 2, 2, '2020-01-01'),
+       ('Câu hỏi JAVA 1', 1, 1, 10, '2021-07-01'),
+       ('Câu hỏi JAVA 2', 1, 2, 5, '2021-06-01'),
+       ('Câu hỏi HTML 1', 3, 1, 2, NOW()),
+       ('Câu hỏi HTML 2', 3, 2, 2, NOW());
+
+/* lenh sua bang xoa khoa ngoai foreign key*/
+ALTER TABLE `Answer`
+    DROP CONSTRAINT `fk_qid`;
+/* INSERT DATA bang Answer */
+INSERT INTO `Answer` (`Content`, `QuestionID`, `isCorrect`)
+VALUES ('Câu trả lời 1 - question SQL 1', 1, 1),
+       ('Câu trả lời 2 - question SQL 1', 1, 0),
+       ('Câu trả lời 3 - question SQL 1', 1, 0),
+       ('Câu trả lời 4 - question SQL 1', 1, 1),
+       ('Câu trả lời 1 - question SQL 2', 2, 0),
+       ('Câu trả lời 2 - question SQL 2', 2, 0),
+       ('Câu trả lời 3 - question SQL 2', 2, 0),
+       ('Câu trả lời 4 - question SQL 2', 2, 1),
+       ('Câu trả lời 1 - question JAVA 1', 3, 0),
+       ('Câu trả lời 2 - question JAVA 1', 3, 1),
+       ('Câu trả lời 1 - question JAVA 2', 4, 0),
+       ('Câu trả lời 2 - question JAVA 2', 4, 0),
+       ('Câu trả lời 3 - question JAVA 2', 4, 0),
+       ('Câu trả lời 4 - question JAVA 2', 4, 1),
+       ('Câu trả lời 1 - question HTML 1', 5, 1),
+       ('Câu trả lời 2 - question HTML 2', 5, 0);
+
+/* lenh sua bang xoa khoa ngoai foreign key*/
+ALTER TABLE `Exam`
+    DROP CONSTRAINT `fk_ex_1`;
+ALTER TABLE `Exam`
+    DROP CONSTRAINT `fk_ex_2`;
+/* INSERT DATA bang Exam */
+INSERT INTO `Exam`(`Code`, `Title`, `CategoryID`, `Duration`, `CreatorID`, `CreateDate`)
+VALUES ('MS_01', 'De thi 01', 1, 90, 1, NOW()),
+       ('MS_02', 'De thi 02', 1, 60, 5, NOW()),
+       ('MS_03', 'De thi 03', 2, 60, 9, NOW()),
+       ('MS_04', 'De thi 04', 2, 90, 1, NOW()),
+       ('MS_05', 'De thi 05', 1, 60, 2, NOW()),
+       ('MS_06', 'De thi 06', 2, 90, 2, NOW()),
+       ('MS_07', 'De thi 07', 1, 60, 1, NOW());
+
+/* lenh sua bang xoa khoa ngoai foreign key*/
+ALTER TABLE `ExamQuestion`
+    DROP CONSTRAINT `fk_eq1`,
+    DROP CONSTRAINT `fk_eq2`;
+/* INSERT DATA bang Examquestion */
+INSERT INTO `ExamQuestion`
+VALUES (1, 1),
+       (2, 1),
+       (3, 1),
+       (4, 1),
+       (5, 2),
+       (6, 2),
+       (7, 2),
+       (8, 2),
+       (9, 3),
+       (10, 3),
+       (11, 4),
+       (12, 4),
+       (13, 4),
+       (14, 4),
+       (15, 5),
+       (16, 5);
+
+
+-- ===================================== Bài tập ======================================
+-- Question 1: Tạo view có chứa danh sách nhân viên thuộc phòng ban sale
+
+Create view list_of_Sale_staff AS
+	Select 		A.AccountID , A.Email , A.Fullname
+	from 		department D
+	inner join `Account` A 
+	on 			D.DepartmentID = A.Accountid
+	where 		D.Departmentid = 5;
+
+-- Question 2: lấy ra thông tin các account tham gia vào nhiều group nhất
+
+/* Cách 1*/
+Select GA.AccountID, A.Fullname, A.username , count(GA.Accountid) as 'Số_Group_tham_gia_là' 
+from groupaccount GA
+inner join `account` A
+on  GA.AccountID = A.AccountID
+group by GA.Accountid
+having count(GA.Accountid) = (select max(checker) from (select GA.AccountID , count(GA.GroupID) as checker
+							  from `groupaccount` GA 
+							  Group by GA.accountid) as maximum);
+
+/* Cách 2*/
+
+with CTE_MaxJoinGroup AS (select GA.AccountID , count(GA.GroupID) as checker
+							  from `groupaccount` GA 
+							  Group by GA.accountid) 
+                              
+Select GA.AccountID, A.Fullname, A.username , count(GA.Accountid) as 'Số_Group_tham_gia_là' 
+from groupaccount GA
+inner join `account` A
+on  GA.AccountID = A.AccountID
+group by GA.Accountid
+having count(GA.Accountid) = (select max(checker) from CTE_MaxJoinGroup); 
+
+
+-- Question 3: Tạo view có chứa câu hỏi có những content quá dài (content quá 300 từ được coi là quá dài) và xóa nó đi
+
+Create View Cau_Hoi_Co_content_dai as
+select QuestionID , Content 
+from Question
+where character_length(Content) >= 7;
+
+
+/*delete
+from Cau_Hoi_Co_content_dai */
+
+-- Question 4: Tạo view có chứa danh sách các phòng ban có nhiều nhân viên nhất
+
+With CTE_So_NV_moi_phong AS (select A.DepartmentID , count(A.DepartmentID) as checker
+									from `account` A 
+									group by A.DepartmentID)
+
+Select A.DepartmentID , D.Departmentname , count(A.DepartmentID) as 'Số nhân viên là' from `account` A 
+inner join department D 
+on A.DepartmentID = D.DepartmentID 
+group by A.DepartmentID 
+having count(A.DepartmentID) = (select max(checker) from CTE_So_NV_moi_phong );
+
+-- Question 5: Tạo view có chứa tất các các câu hỏi do user họ Nguyễn tạo
+with CTE_user_ho_Nguyen AS (Select AccountID  
+							from `Account` A 
+							where fullname like 'Nguyen%')
+
+Select *
+from question Q 
+Where CreatorID in (Select * from CTE_user_ho_Nguyen);
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
